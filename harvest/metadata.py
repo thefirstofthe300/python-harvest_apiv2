d17f6bed6d12dc46998e4ff6c33cbcdb51e0e182
Bradley van Ree <38775942+Brad-eki@users.noreply.github.com>
2020-03-20 21:41:54 +1100

    Bugfix, was importing built-in warnings package
    Co-Authored-By: Bradley van Ree <bradbase@users.noreply.github.com>

--- harvest/metadata.py
+++ harvest/metadata.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __author__ = "Bradley van Ree"
 __copyright__ = "Copyright 2020 Bradbase"
 __maintainer__ = "Bradley van Ree"
